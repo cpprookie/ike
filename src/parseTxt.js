b00801add0d4fb7cb8905f68d6a957175d2f24fc
cpprookie <1153644103@qq.com>
2019-05-12 11:54:46 +0800

    support multiple areas

--- src/parseTxt.js
+++ src/parseTxt.js
@@ -1,21 +1,48 @@
 const fs = require("fs");
 const splitterReg = /[.。, 、,，']/g;
+const SAMPLE_NUM_COL = 'C';
+const PLANS_COL = 'E';
+const isStartWIthNum = s => /^\d/.test(s);
+const isNewHouseArea = s => isStartWIthNum(s) && (s.includes('月') || s.includes('日'));
 
 function parseTxt(txtPath) {
   if (!fs.existsSync(txtPath)) {
     throw new Error("File not exsit!");
   }
-  const array = fs
+  const arrays = fs
     .readFileSync(txtPath)
     .toString()
     .split("\n");
+  let lastRow = 0;
+  const dataArrs = arrays.reduce((resArr, item) => {
+    if (isNewHouseArea(item)) {
+      resArr.push([item])
+    } else {
+      resArr[resArr.length - 1].push(item)
+    }
+    return resArr;
+  }, [])
+  const dataObj = dataArrs.reduce((obj, array) =>
+    Object.assign(obj, parseSingleArea(array, obj.nextStartRow)), { nextStartRow: 0 }
+  );
+  delete dataObj.nextStartRow;
+  return dataObj;
+}
+
+function parseSingleArea(array, startRowNum = 0) {
+  // date A
+  // location B
+  // sampes G
   const [date, location, samples, ...areas] = array;
-  return {
-      'A': [[date]],
-      'B': [[location]],
-      'G': splitPlants(samples),
-      ...parseAreas(areas),
-  }
+  const res = {}
+  res[`A${startRowNum}`] = [[date]];
+  res[`B${startRowNum}`] = [[location]];
+  res[`G${startRowNum}`] = splitPlants(samples);
+  Object.assign(res, parseAreas(areas, startRowNum));
+  // find endRowNum
+  const maxRows = Math.max(...Object.keys(res).filter(k => k.includes(PLANS_COL || 'G')).map(p => parseInt(p.slice(1)) + res[p].length));
+  res['nextStartRow'] = maxRows + 1;
+  return res;
 }
 
 const splitPlants = plants => plants.split('，').filter(s => s.length > 0).map(x => [x]);
@@ -26,26 +53,29 @@ const findSpliter = s => {
     }
   }
 }
-function parseAreas(areas) {
-  const res = {
-    'E': [],
-  };
+
+function parseAreas(areas, startRowNum) {
+  // 标号为0的样方无植物时，E列从编号1开始
+  const plantOriginKey = isStartWIthNum(areas[1]) ? `${PLANS_COL}${1+startRowNum}` : `${PLANS_COL}${startRowNum}`;
+  const res = {};
+  res[plantOriginKey] = [];
   let lastDNum = 0, lastELen = 1;
   areas.forEach((origin, index) => {
     const item = origin.replace(/\r/g, '');
-    const k = /^\d/.test(item) ? 'C' : 'E';
-    if (k === 'C') {
+    // column C & D
+    if (isStartWIthNum(item)) {
       lastDNum += index === 0 ? 0 : lastELen;
       try {
-        const splitterIndex = findSpliter(item)
-        res[k+lastDNum] = [[item.slice(0, splitterIndex), item.slice(splitterIndex + 1).replace(splitterReg, '')]];
+        const splitterIndex = findSpliter(item);
+        res[SAMPLE_NUM_COL+ (lastDNum + startRowNum)] = [[parseInt(item.slice(0, splitterIndex), 10), item.slice(splitterIndex + 1).replace(splitterReg, '')]];
       } catch(e) {
-        console.log(e.message);
+        console.log(e);
       }
     } else {
+      // column E
       const plants = splitPlants(item);
       lastELen = plants.length;
-      res['E'].push(...plants);      
+      res[plantOriginKey].push(...plants);      
     }
   })
   return res;
--- src/writeExcel.js
+++ src/writeExcel.js
@@ -2,29 +2,53 @@ const XLSX = require('xlsx');
 const parseTxt = require('./parseTxt');
 const path = require('path');
 
+function initEmptyWorkSheet() {
+    const wb = XLSX.utils.book_new();
+    const wsName = "survey1";
+    /* make worksheet */
+    const wsData = [['日期', '小区名称', '样方编号', '生境', '名称', '是否开花', '样方旁种植植物']];
+    const ws = XLSX.utils.aoa_to_sheet(wsData);
+
+    /* Add the worksheet to the workbook */
+    XLSX.utils.book_append_sheet(wb, ws, wsName);
+    return { wb, ws };
+}
+
+function getFileName() {
+    const date = new Date();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    return `survey_${month}_${day}.xlsx`;
+}
+
 function getNextRow(ws) {
     // empty ws condition start from 0
-    const {s, e} = XLSX.utils.decode_range(workSheet['!ref']);
+    const {e} = XLSX.utils.decode_range(ws['!ref']);
     return e.r + 2;
 }
 
-function insertRow(ws, txtObj, rowNumber) {
-    console.log(txtObj);
+function insertRow(wb, ws, fileName, txtObj, rowNumber) {
     const keys = Object.keys(txtObj);
     keys.forEach(key => {
         const origin = key[0] + (key.length > 1 ? (parseInt(key.slice(1)) + rowNumber) : rowNumber);
+        if (key.includes('E')) { console.log(key, origin) }
         XLSX.utils.sheet_add_aoa(ws, txtObj[key], { origin });
     })
-    XLSX.writeFile(workbook, 'abc.xlsx')
+    XLSX.writeFile(wb, fileName);
 }
 
-// init workSheet
-const workbook = XLSX.readFile(path.join(__dirname, 'test.xlsx'));
-const firstSheetName = workbook.SheetNames[0];
-console.log(firstSheetName);
-const workSheet = workbook.Sheets[firstSheetName];
-console.log(workSheet);
-const txt2aoa = parseTxt(path.join(__dirname, 'ke.txt'));
-// console.log(txt2aoa);
-const nextRow = getNextRow(workSheet);
-insertRow(workSheet, txt2aoa, nextRow);
+function writeExcel(txtPath) {
+    const { wb, ws } = initEmptyWorkSheet();
+    const nextRow = getNextRow(ws);
+    const fileName = getFileName();
+    const txt2aoa = parseTxt(txtPath);
+    insertRow(wb, ws, fileName, txt2aoa, nextRow);
+}
+
+// run main thread
+try {
+    const txtPath = path.join(__dirname, 'ke.txt')
+    writeExcel(txtPath);
+} catch(e) {
+    console.error(e);
+}
